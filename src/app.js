91fc14b522fc5907a6fb44215c225699380143a0
Igor de Moraes <r.de.moraes.igor@gmail.com>
2022-11-07 17:07:50 -0300

    Arrays users e tweets começam vazias

--- src/app.js
+++ src/app.js
@@ -6,72 +6,8 @@ app.use(cors());
 app.use(express.json());
 
 const users = [
-    {
-        username: 'bobesponja', 
-        avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info" 
-    },
-    {
-        username: 'patrick', 
-        avatar: "https://upload.wikimedia.org/wikipedia/pt/b/b1/Patrick_Estrela.png" 
-    },
-    {
-        username: 'lula', 
-        avatar: "https://upload.wikimedia.org/wikipedia/pt/0/0c/Squidward_Tentacles.png" 
-    },
-    {
-        username: 'sirigueijo', 
-        avatar: "https://static.wikia.nocookie.net/wikiesponja/images/2/2d/Sr_Sirigueijo_de_Bob_Esponja.png/revision/latest?cb=20170325000158&path-prefix=pt-br" 
-    }
 ];
 const tweets = [
-    {
-        username: "bobesponja",
-        tweet: "eu amo o hub"
-    },
-    {
-        username: "bobesponja",
-        tweet: "eu sou uma esponja"
-    },
-    {
-        username: "bobesponja",
-        tweet: "eu moro num abacaxi"
-    },
-    {
-        username: "patrick",
-        tweet: "eu sou uma estrela"
-    },
-    {
-        username: "patrick",
-        tweet: "eu moro numa pedra"
-    },
-    {
-        username: "patrick",
-        tweet: "eu sou rosa"
-    },
-    {
-        username: "lula",
-        tweet: "eu sou uma lula"
-    },
-    {
-        username: "lula",
-        tweet: "eu sou rabugento"
-    },
-    {
-        username: "lula",
-        tweet: "minha casa é a minha cara"
-    },
-    {
-        username: "sirigueijo",
-        tweet: "eu sou um siri"
-    },
-    {
-        username: "sirigueijo",
-        tweet: "eu sou pai de uma baleia"
-    },
-    {
-        username: "sirigueijo",
-        tweet: "eu sou mão de vaca"
-    }
 ];
 
 app.post('/sign-up', (req, res) => {
