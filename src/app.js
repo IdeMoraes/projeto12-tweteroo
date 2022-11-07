763613208a27ca5ddac8982343e8450c566cdb70
Igor de Moraes <r.de.moraes.igor@gmail.com>
2022-11-07 04:26:51 -0300

    POST /tweets criado

--- src/app.js
+++ src/app.js
@@ -4,10 +4,17 @@ const app = express();
 app.use(express.json());
 
 const users = [];
+const tweets = [];
 
 app.post('/sign-up', (req, res) => {
     const user = req.body;
     users.push(user);
+    res.send("OK");
+});
+
+app.post('/tweets', (req, res) => {
+    const tweet = req.body;
+    tweets.push(tweet);
     res.send("OK");
 });
 
