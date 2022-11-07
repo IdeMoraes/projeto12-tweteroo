d3ed182bd8705fff9f0d0f3455d880de585babfb
Igor de Moraes <r.de.moraes.igor@gmail.com>
2022-11-07 04:24:23 -0300

    POST /sign-up criado

--- src/app.js
+++ src/app.js
@@ -1,5 +1,14 @@
 import express from 'express';
 
 const app = express();
+app.use(express.json());
+
+const users = [];
+
+app.post('/sign-up', (req, res) => {
+    const user = req.body;
+    users.push(user);
+    res.send("OK");
+});
 
 app.listen(5005);
